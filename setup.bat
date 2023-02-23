5c2b7dc968b7e261d9121ee6f0b84419a7511c65
abhishekkrbaliase <abhishekkrbaliase@gmail.com>
2023-02-23 12:26:59 +0530

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,6 @@
+choco install anaconda3
+conda create -n scrapper python==3.7 -y
+conda activate scrapper
+pip3 install -U selenium==4.7.2
+pip3 install pandas==1.1.5
+pip3 install webdriver-manager==3.8.5
