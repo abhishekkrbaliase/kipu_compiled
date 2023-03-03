a8335b12a6a1ed37ce432ea4260d11e80c615f8f
abhishekkrbaliase <abhishekkrbaliase@gmail.com>
2023-03-03 12:30:31 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -3,4 +3,4 @@ conda create -n scrapper python==3.7 -y
 conda activate scrapper
 pip3 install -U selenium==4.7.2
 pip3 install webdriver-manager==3.8.5
-conda install pandas==1.3.5
+conda install pandas==1.3.5 -y
