0cfc78ccd63368aeb294e77ed05dc694054e1020
abhishekkrbaliase <abhishekkrbaliase@gmail.com>
2023-03-08 19:48:32 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,5 @@
-choco install anaconda3
-conda create -n scrapper python==3.7 -y
-conda activate scrapper
-pip3 install -U selenium==4.7.2
-pip3 install webdriver-manager==3.8.5
-conda install pandas==1.3.5 -y
+call conda create -n scrapper python==3.7 -y
+call conda activate scrapper
+call pip3 install -U selenium==4.7.2
+call pip3 install webdriver-manager==3.8.5
+call conda install pandas==1.3.5 -y
