b7f0dacd57edbf67079925a6da4c417bd8330ec9
abhishekkrbaliase <abhishekkrbaliase@gmail.com>
2023-06-12 15:42:40 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,5 +2,5 @@ call conda create -n scrapper python==3.7 -y
 call conda activate scrapper
 call pip3 install -U selenium==4.7.2
 call pip3 install webdriver-manager==3.8.5
-call pip3 install %cd%\lib\kipuUtils-0.0.1-py3-none-any.whl
 call conda install pandas==1.3.5 -y
+call pip3 install %cd%\lib\kipuUtils-0.0.1-py3-none-any.whl
