dee7db3c74f11d940f387e23dca59eeee030fd71
abhishekkrbaliase <abhishekkrbaliase@gmail.com>
2023-09-23 11:36:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ pip3 install -U selenium==4.7.2
 pip3 install webdriver-manager==3.8.5
 pip3 install chromedriver-py==116.0.5845.96
 conda install pandas==1.3.5 -y
-pip3 install %cd%\lib\kipuUtils-0.0.2-py3-none-any.whl
+pip3 install lib/kipuUtils-0.0.2-py3-none-any.whl
